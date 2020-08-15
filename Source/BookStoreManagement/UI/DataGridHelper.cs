1091e27a21f60d4ebb0ddc6c736c02a1e3448849
Dat Ho <thaidatdh@gmail.com>
2020-08-15 19:24:51 +0700

    Revert HA's Commit 2711c47 "Revert "update transaction management deleted records status""
    This reverts commit 2711c4752e970f0cc0c0089ed8ff41ab760a3c9a.

---
+++ Source/BookStoreManagement/UI/DataGridHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace BookStoreManagement.UI
+{
+   public class DataGridHelper
+   {
+      public static TextDecorationCollection GetTextDecoration(DependencyObject obj)
+      {
+         return (TextDecorationCollection)obj.GetValue(TextDecorationProperty);
+      }
+
+      public static void SetTextDecoration(DependencyObject obj, TextDecorationCollection value)
+      {
+         obj.SetValue(TextDecorationProperty, value);
+      }
+
+      public static readonly DependencyProperty TextDecorationProperty =
+        DependencyProperty.RegisterAttached("TextDecoration", typeof(TextDecorationCollection), typeof(DataGridHelper), new FrameworkPropertyMetadata(
+          null,
+          FrameworkPropertyMetadataOptions.AffectsRender | FrameworkPropertyMetadataOptions.Inherits,
+          new PropertyChangedCallback((d, e) =>
+          {
+             TextBlock textBlock = d as TextBlock;
+             if (textBlock != null)
+             {
+                textBlock.TextDecorations = e.NewValue as TextDecorationCollection;
+             }
+          })));
+   }
+}
